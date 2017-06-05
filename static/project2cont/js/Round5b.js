ed8122ab36a3bfe5569182e17bcc1272f3fe78de
kathi126 <dakommtfreudeauf@hotmail.com>
2017-06-05 08:06:07 +0200

    Update Round5b.js

--- static/project2cont/js/Round5b.js
+++ static/project2cont/js/Round5b.js
@@ -49,7 +49,7 @@ me.check = function(x) {
     el.value = document.getElementById("min").innerHTML + ":" + document.getElementById("sec").innerHTML;
     el.id = el.name
 
-    var answers = document.getElementById("id_output5b");
+    var answers = document.getElementById("id_output5");
     answers.appendChild(el);
 
     if (x === solutions_5b[me.index]) {
@@ -69,7 +69,7 @@ me.makeguess = function(x) {
         var string = document.getElementById("string");
         string.innerHTML = sequences_5b[x];
 
-        document.getElementById("id_output5b").setAttribute("value",me.index);
+        document.getElementById("id_output5").setAttribute("value",me.index);
     }
 }
 
